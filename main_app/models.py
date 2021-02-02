59ac09ac1769ff4791139423cef854b68a8bcd91
mcholbrook <mirandacholbrook@gmail.com>
2021-02-02 15:25:53 -0500

    Fix error with profile model

--- main_app/models.py
+++ main_app/models.py
@@ -5,9 +5,10 @@ from datetime import date
 # Create your models here.
 
 class Profile(models.Model):
-  bio = models.TextField(max_length=200)
-  photo = models.CharField(max_length=200)
+  name = models.CharField(max_length=50)
+  bio = models.TextField(max_length=200, blank=True)
+  photo = models.CharField(max_length=200, blank=True)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
-    return f"Profile for user_id: {self.user_id}"
+    return f"Profile for user: {self.name} at user_id {self.user_id}."
